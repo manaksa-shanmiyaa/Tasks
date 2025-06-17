6a14701d2cc4b4eb3100d1c749c542a66c5fd141
manaksa-shanmiyaa <manumahivr@gmail.com>
2025-06-17 15:17:56 +0530

    day2 (2).java

--- day2 (2).java
+++ day2 (2).java
@@ -1,14 +1,3 @@
-import java.util.Scanner;
-
-public class ReverseArray {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-       
-        System.out.print("Enter number of elements: ");
-        int n = scanner.nextInt();
-
-        int[] numbers = new int[n];
 task 1
        
         System.out.println("Enter the numbers:");
@@ -25,4 +14,40 @@ task 1
         scanner.close();
     }
 }
+task 2
+
+class Main {
+    public static void main(String[] args) {
+        String name = "manut";
+        int n= name.length();
+        int count= 0;
+        int count1=0;
+        for(int i=0;i<n;i++){
+            System.out.println(name.charAt(i));
+            if(name.charAt(i)=='a' || name.charAt(i)=='i' || name.charAt(i)=='e' || name.charAt(i)=='o' || name.charAt(i)=='u')
+                count++;
+       
+           else if(name.charAt(i)>='a'&& name.charAt(i)<='z') {
+               count1++;
+           
+               
+           }
+               
+        }
+       
+        System.out.println(count);
+        System.out.println(count1);
+        
+               
+               
+                }
+}
+               
+       // System.out.println(name.charAt(0));
+       
+       
+       
+       // System.out.println(name);
+
+   
 
